ce0fa80ad3de782295643ce95646964ec2d332a4
LuckyLuke00 <Lucas.Kinoo5@gmail.com>
2022-11-10 16:40:00 +0100

    Implement CreateNavigationGraph

--- GPP_Framework_Rewrite/source/framework/EliteAI/EliteGraphs/ENavGraph.cpp
+++ GPP_Framework_Rewrite/source/framework/EliteAI/EliteGraphs/ENavGraph.cpp
@@ -32,6 +32,8 @@ Elite::NavGraph::~NavGraph()
 {
 	delete m_pNavMeshPolygon;
 	m_pNavMeshPolygon = nullptr;
+
+	// Cleanup the connections
 }
 
 int Elite::NavGraph::GetNodeIdxFromLineIdx(int lineIdx) const
@@ -50,9 +52,60 @@ Elite::Polygon* Elite::NavGraph::GetNavMeshPolygon() const
 	return m_pNavMeshPolygon;
 }
 
+// TODO: Test implementation
 void Elite::NavGraph::CreateNavigationGraph()
 {
 	//1. Go over all the edges of the navigationmesh and create nodes
+	
+	// Loop over all the lines (Tip: use GetLines()) of the Polygon
+	// For each line:
+	for (const auto& line : m_pNavMeshPolygon->GetLines())
+	{
+		// Check if that line is connected to another triangle (Tip: GetTrianglesFromLineIndex)
+		if (m_pNavMeshPolygon->GetTrianglesFromLineIndex(line->index).size() > 1)
+		{
+			// Create a NavGraphNode on the graph
+				// Position it in the middle of the line
+				// And give it the lineIdx
+			AddNode(new NavGraphNode(GetNextFreeNodeIndex(), line->index, (line->p1 + line->p2) / 2.0f));
+		}
+	}
+
+	// For each Triangle in the Navigation Mesh, find the nodes.
+	for (const auto& triangle : m_pNavMeshPolygon->GetTriangles())
+	{
+		// Variable that stores the valid node indices reserve 3 indices
+		std::vector<int> validNodeIndices;
+		validNodeIndices.reserve(3);
+		
+		// Loop over the line indexes (Tip: Look at the metadata property of Triangle)
+		for (const auto& lineIdx : triangle->metaData.IndexLines)
+		{
+			// Check if a valid NavGraphNode for that lineIdx exists (Tip: GetNodeIdxFromLineIdx)
+			if (GetNodeIdxFromLineIdx(lineIdx) != invalid_node_index)
+			{
+				// If so, add it to the validNodeIndices
+				validNodeIndices.push_back(GetNodeIdxFromLineIdx(lineIdx));
+			}
+		}
+
+		// If there are 2 valid nodes, create one connection between them
+		// Use the function AddConnection
+		if (validNodeIndices.size() == 2)
+		{
+			AddConnection(new GraphConnection2D(validNodeIndices[0], validNodeIndices[1]));
+		}
+
+		if (validNodeIndices.size() == 3)
+		{
+			AddConnection(new GraphConnection2D(validNodeIndices[0], validNodeIndices[1]));
+			AddConnection(new GraphConnection2D(validNodeIndices[1], validNodeIndices[2]));
+			AddConnection(new GraphConnection2D(validNodeIndices[2], validNodeIndices[0]));
+		}
+	}
+
+	// Set the connection’s costs to the distance (Tip:SetConnectionCostsToDistance)
+	SetConnectionCostsToDistance();
 
 	//2. Create connections now that every node is created
 
